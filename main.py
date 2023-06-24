93b130145a89ca34f1e91546e1c31ec11f8e11a6
Nguyễn Phan Huỳnh Đức <nguyenphanhuynhduc@Nguyens-MacBook-Air.local>
2023-06-24 23:52:33 +0700

    done

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-"""
-A simple app to create a JWT token.
-"""
 import os
 import logging
 import datetime
 import functools
-import jwt
+import jose.jwt as jwt
 
-# pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
 
@@ -38,22 +33,24 @@ LOG = _logger()
 LOG.debug("Starting with log level: %s" % LOG_LEVEL )
 APP = Flask(__name__)
 
+
 def require_jwt(function):
     """
-    Decorator to check valid jwt is present.
+    Decorator to check valid JWT is present.
     """
     @functools.wraps(function)
-    def decorated_function(*args, **kws):
-        if not 'Authorization' in request.headers:
+    def decorated_function(*args, **kwargs):
+        if 'Authorization' not in request.headers:
             abort(401)
         data = request.headers['Authorization']
         token = str.replace(str(data), 'Bearer ', '')
         try:
             jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-        except: # pylint: disable=bare-except
+        except jwt.JWTError:
             abort(401)
 
-        return function(*args, **kws)
+        return function(*args, **kwargs)
+
     return decorated_function
 
 
@@ -64,7 +61,6 @@ def health():
 
 @APP.route('/auth', methods=['POST'])
 def auth():
-    #hello
     request_data = request.get_json()
     email = request_data.get('email')
     password = request_data.get('password')
@@ -78,36 +74,31 @@ def auth():
 
     user_data = body
 
-    return jsonify(token=_get_jwt(user_data).decode('utf-8'))
+    return jsonify(token=_get_jwt(user_data))
 
 
 @APP.route('/contents', methods=['GET'])
+@require_jwt
 def decode_jwt():
-    """
-    Check user token and return non-secret data
-    """
-    if not 'Authorization' in request.headers:
+    if 'Authorization' not in request.headers:
         abort(401)
     data = request.headers['Authorization']
     token = str.replace(str(data), 'Bearer ', '')
     try:
         data = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    except: # pylint: disable=bare-except
+    except jwt.JWTError:
         abort(401)
 
-
-    response = {'email': data['email'],
-                'exp': data['exp'],
-                'nbf': data['nbf'] }
+    response = {'email': data['email'], 'exp': data['exp'], 'nbf': data['nbf']}
     return jsonify(**response)
 
 
 def _get_jwt(user_data):
     exp_time = datetime.datetime.utcnow() + datetime.timedelta(weeks=2)
-    payload = {'exp': exp_time,
-               'nbf': datetime.datetime.utcnow(),
-               'email': user_data['email']}
-    return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+    payload = {'exp': exp_time, 'nbf': datetime.datetime.utcnow(), 'email': user_data['email']}
+    encoded_jwt = jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+    return encoded_jwt
+
 
 if __name__ == '__main__':
     APP.run(host='127.0.0.1', port=8080, debug=True)
